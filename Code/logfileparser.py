ccbfc16b2c9a21eac51ed814bc35f042fcf01a65
Yves Lütjens <yves@luetjens.ch>
2019-10-21 22:38:49 +0200

    backing up custom parser

--- Code/logfileparser.py
+++ Code/logfileparser.py
@@ -10,11 +10,10 @@ The four steps are:
 
 '''
 
-# Remember to update the script for the new data when you change this URL
 URL = "../Logfiles/Labeled/normalTrafficTraining.csv"
 #URL = "../Logfiles/Unlabeled/access.log01.txt"
 
-from pandas import read_csv
+import pandas as pa
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -27,15 +26,10 @@ except ImportError:
 # =====================================================================
 
 def download_data():
-    frame = read_csv(
+    frame = pa.read_csv(
         URL,
-        
-        # Uncomment if the file needs to be decompressed
-        #compression='gzip',
-        #compression='bz2',
 
         # Specify the file encoding
-        # Latin-1 is common for data from US sources
         encoding='utf-8',
 
         # Specify the separator in the data
@@ -45,38 +39,56 @@ def download_data():
 
         # Ignore spaces after the separator
         skipinitialspace=True,
-
         # Generate row labels from each row number
-        index_col=0,
+        #index_col=0,
         #index_col=0,       # use the first column as row labels
         #index_col=-1,      # use the last column as row labels
-
+    
         # Generate column headers row from each column number
         header=None,
         #header=0,          # use the first line as headers
 
         # Use manual headers and skip the first row in the file
         #header=0,
-        #names=['col1', 'col2', ...],
+        names=['attributes', 'values']
     )
     
+
     # Return a subset of the columns
     #return frame[['col1', 'col4', ...]]
+    
     print(frame.head())
+    
     # Return the entire frame
     return frame
 
 # =====================================================================
+
+def read_data():
+    data = []
+    file = open(URL, "r")
+
+    if file.mod == 'r':
+        lines = file.readlines
+        isreadingblock = False
+        wasemptyline = False
+
+        for line in lines:
+            if isreadingblock and line == "":
+                wasemptyline = True
+            if line=="":
+                continue
+            el
+    else:
+        print("File could not be opened")
+
 
 
 if __name__ == '__main__':
     # Loading the data set from URL
     print("Loading data from {}".format(URL))
     frame = download_data()
-
+    dict = read_data()
     # Process data into feature and label arrays
     print("Processing {} samples with {} attributes".format(len(frame.index), len(frame.columns)))
 
-    # Display the results
-    print("Printing the results")
-    print(frame)
