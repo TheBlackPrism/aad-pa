63188f1ca6ff3deff718cdae0a2d8ae98e3e35bb
Yves Lütjens <yves@luetjens.ch>
2019-10-21 21:28:29 +0200

    Added first functioning version of a logfile parser for labeled datasets

---
+++ Code/classifier.py
@@ -0,0 +1,82 @@
+'''
+This script performs the basic process for applying a machine learning
+algorithm to a dataset using Python libraries.
+
+The four steps are:
+   1. Download a dataset (using pandas)
+   2. Process the numeric data (using numpy)
+   3. Train and evaluate learners (using scikit-learn)
+   4. Plot and compare results (using matplotlib)
+
+'''
+
+# Remember to update the script for the new data when you change this URL
+URL = "../Logfiles/Labeled/normalTrafficTraining.csv"
+#URL = "../Logfiles/Unlabeled/access.log01.txt"
+
+from pandas import read_csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+try:
+    # [OPTIONAL] Seaborn makes plots nicer
+    import seaborn
+except ImportError:
+    pass
+
+# =====================================================================
+
+def download_data():
+    frame = read_csv(
+        URL,
+        
+        # Uncomment if the file needs to be decompressed
+        #compression='gzip',
+        #compression='bz2',
+
+        # Specify the file encoding
+        # Latin-1 is common for data from US sources
+        encoding='utf-8',
+
+        # Specify the separator in the data
+        sep=',',            # comma separated values
+        #sep='\t',          # tab separated values
+        #sep=' ',           # space separated values
+
+        # Ignore spaces after the separator
+        skipinitialspace=True,
+
+        # Generate row labels from each row number
+        index_col=0,
+        #index_col=0,       # use the first column as row labels
+        #index_col=-1,      # use the last column as row labels
+
+        # Generate column headers row from each column number
+        header=None,
+        #header=0,          # use the first line as headers
+
+        # Use manual headers and skip the first row in the file
+        #header=0,
+        #names=['col1', 'col2', ...],
+    )
+    
+    # Return a subset of the columns
+    #return frame[['col1', 'col4', ...]]
+    print(frame.head())
+    # Return the entire frame
+    return frame
+
+# =====================================================================
+
+
+if __name__ == '__main__':
+    # Loading the data set from URL
+    print("Loading data from {}".format(URL))
+    frame = download_data()
+
+    # Process data into feature and label arrays
+    print("Processing {} samples with {} attributes".format(len(frame.index), len(frame.columns)))
+
+    # Display the results
+    print("Printing the results")
+    print(frame)
