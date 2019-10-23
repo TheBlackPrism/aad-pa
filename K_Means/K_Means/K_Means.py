b23e6c728147f7fc2f513c68875c643443cef2ff
kaelilor <kaelilor@students.zhaw.ch>
2019-10-23 19:02:38 +0200

    add K_Means class and constructor

---
+++ K_Means/K_Means/K_Means.py
@@ -0,0 +1,9 @@
+class K_Means:
+    """description of class"""
+    
+    def __init__(self, k = 3, tolerance = 0.0001, max_iterations = 500):
+        self.k = k
+        self.tolerance = tolerance
+        self.max_iterations = max_iterations
+
+
