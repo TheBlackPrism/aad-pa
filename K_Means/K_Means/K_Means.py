f1a6b5f9bc47d39d8b03d6e18dccacc19e16036e
kaelilor <kaelilor@students.zhaw.ch>
2019-10-23 19:08:38 +0200

    Add Euclidean_distance method to compute the distance for two features p and q

--- K_Means/K_Means/K_Means.py
+++ K_Means/K_Means/K_Means.py
@@ -1,3 +1,5 @@
+import math
+
 class K_Means:
     """description of class"""
     
@@ -5,5 +7,17 @@ class K_Means:
         self.k = k
         self.tolerance = tolerance
         self.max_iterations = max_iterations
+    
+    #Compute euclidean distance for two features p and q
+    def Euclidean_distance(p,q):
+        squared_distance = 0
 
+        if len(p) != len(q):
+            return 0
+        
+        for i in range(len(p)):
+            squared_distance += (p[i] - q[i])**2
+
+        distance = squared_distance(squared_distance)
+        return distance
 
