9ad6b0e0290a03340faa8aaadd8865ff3c5620dd
Yves Lütjens <yves@luetjens.ch>
2019-11-02 13:38:46 +0100

    Commented methods for better understanding

--- Code/NGramm.py
+++ Code/NGramm.py
@@ -1,4 +1,5 @@
 import re
+import numpy as np
 import K_Means
 import logfileparser as parser
 
@@ -11,21 +12,27 @@ class NGramm():
         self.ngramms_probability = {}
 
     def fit(self, data):
+        """Reads a set of requests and stores probabilities and occurences in the class
+        """
         self.ngramms = self.get_ngramms_for_all(data)
         self.total_number_ngramms = sum(self.ngramms.values())
 
         for ngramm in self.ngramms:
             self.ngramms_probability[ngramm] = float(self.ngramms[ngramm]) / self.total_number_ngramms
 
-    def get_probability_of_request(self, data):
-        ngramms = get_ngramms_for_request(data)
+    def get_probability_of_ngrammset(self, ngramms):
+        """Returns the probability from a set of ngramms
+        """
         total_probability = 0
         for ngramm in ngramms:
             probability_ngramm = self.ngramms_probability.get(ngramm)
             total_probability += probability_ngramm
         return total_probability / len(ngramms)
 
+
     def get_ngramms_for_all(self, data):
+        """Returns a set of N-Gramms from a set of requests
+        """
         ngramms = {}
         normalized_requests = []
 
@@ -42,6 +49,8 @@ class NGramm():
         return ngramms
 
     def get_ngramms_for_request(self, request):
+        """Returns a set of N-Gramms for a single request
+        """
         ngramms = {}
         normalized_request = normalize_request(request['Request'])
         for i in range(len(request)):
@@ -51,8 +60,22 @@ class NGramm():
             else:
                 ngramms[ngramm] = 1
         return ngramms
+    
+    def get_feature_vectors(self, data):
+        """Get a set of two dimensional feature vectors
+        with probability as one axis and the occurences of ngramms as the other axis.
+        """
+        vectors = np.zeros([len(data)])
+        for request in data:
+            print(request)
+            ngramms = self.get_ngramms_for_request(request)
+            probability = self.get_probability_of_ngrammset(ngramms)
+            occurences = sum(ngramms.values())
+            vectors.append([probability, occurence])
 
 def normalize_request(request):
+    """Normalizes a request by replacing all alphanumeric characters with @
+    """
     regex = re.compile(r"[a-zA-Z0-9]+")
     replaced = re.sub(regex, '@',request)
     regex = re.compile(r"\n")
@@ -86,8 +109,9 @@ def main():
     test_clean = parser.read_data('../Logfiles/Labeled/normalTrafficTest.txt')
     test_anomalous = parser.read_data('../Logfiles/Labeled/anomalousTrafficTest.txt')
 
+    ng.get_feature_vectors(training_data)
+
     km = K_Means.K_Means()
-    km.fit()
 
 if __name__ == "__main__":
     main()
