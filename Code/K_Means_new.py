8d9fe68e22f965d3757e274175303b718bb6967e
kaelilor <kaelilor@students.zhaw.ch>
2019-11-04 15:52:11 +0100

    correct indents

--- Code/K_Means_new.py
+++ Code/K_Means_new.py
@@ -14,79 +14,79 @@ class K_Means_new(object):
         self.max_iterations = max_iterations
         self.centroids = {}
 
-    def main():
-        # Reading Data
-        training_data = parser.read_data('../Logfiles/Labeled/normalTrafficTraining.txt')
-        test_clean = parser.read_data('../Logfiles/Labeled/normalTrafficTest.txt')
-        test_anomalous = parser.read_data('../Logfiles/Labeled/anomalousTrafficTest.txt')
-        training_data = parser.read_data('../Logfiles/Labeled/normalTrafficTraining.txt')
+def main():
+    # Reading Data
+    training_data = parser.read_data('../Logfiles/Labeled/normalTrafficTraining.txt')
+    test_clean = parser.read_data('../Logfiles/Labeled/normalTrafficTest.txt')
+    test_anomalous = parser.read_data('../Logfiles/Labeled/anomalousTrafficTest.txt')
+    training_data = parser.read_data('../Logfiles/Labeled/normalTrafficTraining.txt')
 
-        # Training the N-Gramm extractor
-        ng = NGramm()
-        ng.fit(training_data)
+    # Training the N-Gramm extractor
+    ng = NGramm()
+    ng.fit(training_data)
     
-        print("N-Gramms extracted!")
-        print("**************************")
-        print("Starting K-Means Fitting...")
+    print("N-Gramms extracted!")
+    print("**************************")
+    print("Starting K-Means Fitting...")
 
 
 
 
-        # Getting Feature Vectors
-        training_vectors = ng.get_feature_vectors(training_data)
-        test_vectors_clean = ng.get_feature_vectors(test_clean)
-        test_vectors_anomalous = ng.get_feature_vectors(test_anomalous)
+    # Getting Feature Vectors
+    training_vectors = ng.get_feature_vectors(training_data)
+    test_vectors_clean = ng.get_feature_vectors(test_clean)
+    test_vectors_anomalous = ng.get_feature_vectors(test_anomalous)
 
-        print("\n**************************")
-        print("Training model:")
-        print("k = %d" %k)
+    print("\n**************************")
+    print("Training model:")
+    print("k = %d" %k)
 
-        #fit trainings data to obtain clusters
-        kmeans = KMeans(k)
-        clusters = kmeans.fit(training_vectors)
+    #fit trainings data to obtain clusters
+    kmeans = KMeans(k)
+    clusters = kmeans.fit(training_vectors)
 
 
-        for i in range(len(clusters)):
-            if cluster[i] == 1:
-                cluster1 = plt.scatter(request_lengths[i], c = 'r', marker = 'x')
-            elif clusters[i] == 0:
-                cluster2 = plt.scatter(request_lengths[i], c = 'g', marker = 'o')
+    for i in range(len(clusters)):
+        if cluster[i] == 1:
+            cluster1 = plt.scatter(request_lengths[i], c = 'r', marker = 'x')
+        elif clusters[i] == 0:
+            cluster2 = plt.scatter(request_lengths[i], c = 'g', marker = 'o')
             
 
-        #test clean data
-        print("Training done! Switch to testing.")
-        print("**************************")
-        print("Testing normal traffic:")
+    #test clean data
+    print("Training done! Switch to testing.")
+    print("**************************")
+    print("Testing normal traffic:")
   
-        result_clean = kmeans.predict(test_vectors_clean)
-        result_anomalous = kmeans.predict(test_vectors_anomalous)
+    result_clean = kmeans.predict(test_vectors_clean)
+    result_anomalous = kmeans.predict(test_vectors_anomalous)
 
-        print("Predicting successful!")    
-        print("**************************")
-        print("Results:")
+    print("Predicting successful!")    
+    print("**************************")
+    print("Results:")
 
-        # Evaluation
-        accuracy_anomalous = np.count_nonzero(result_anomalous == -1) / len(result_anomalous) * 100
-        accuracy_clean = np.count_nonzero(result_clean == 1) / len(result_clean) * 100
+    # Evaluation
+    accuracy_anomalous = np.count_nonzero(result_anomalous == -1) / len(result_anomalous) * 100
+    accuracy_clean = np.count_nonzero(result_clean == 1) / len(result_clean) * 100
 
-        print("True Positiv: %d %%" % accuracy_anomalous)
-        print("False Positiv: %d %%" % (100 - accuracy_clean))
-        print("Accuracy: %d %%" % ((accuracy_anomalous * len(result_anomalous) + accuracy_clean * len(result_clean)) / (len(result_clean) + len(result_anomalous))))
+    print("True Positiv: %d %%" % accuracy_anomalous)
+    print("False Positiv: %d %%" % (100 - accuracy_clean))
+    print("Accuracy: %d %%" % ((accuracy_anomalous * len(result_anomalous) + accuracy_clean * len(result_clean)) / (len(result_clean) + len(result_anomalous))))
     
-        # Plotting Vectors
-        fig, ax = plt.subplots()
-        samples = 300
-        ax.scatter(training_vectors[:samples,0], training_vectors[:samples,1], s=200,color = "g", alpha = 0.5, label = "Trainings Data")
-        ax.scatter(test_vectors_clean[:samples,0], test_vectors_clean[:samples,1], s=150, color = "b", alpha = 0.5, label = "Clean Data")
-        ax.scatter(test_vectors_anomalous[:samples,0], test_vectors_anomalous[:samples,1], s=100, color = "r", alpha = 0.5, label = "Anomalous Data")
-        plt.xlabel("Probability of the Request")
-        plt.ylabel("Number of N-Gramms Occurences")
-        plt.title("Distribution of Feature Vectors")
-        ax.legend()
-        plt.show()
+    # Plotting Vectors
+    fig, ax = plt.subplots()
+    samples = 300
+    ax.scatter(training_vectors[:samples,0], training_vectors[:samples,1], s=200,color = "g", alpha = 0.5, label = "Trainings Data")
+    ax.scatter(test_vectors_clean[:samples,0], test_vectors_clean[:samples,1], s=150, color = "b", alpha = 0.5, label = "Clean Data")
+    ax.scatter(test_vectors_anomalous[:samples,0], test_vectors_anomalous[:samples,1], s=100, color = "r", alpha = 0.5, label = "Anomalous Data")
+    plt.xlabel("Probability of the Request")
+    plt.ylabel("Number of N-Gramms Occurences")
+    plt.title("Distribution of Feature Vectors")
+    ax.legend()
+    plt.show()
 
 if __name__ == "__main__":
-    main()
+        main()
 
 
 
