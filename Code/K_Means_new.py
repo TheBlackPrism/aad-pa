60928f6c3ef87bf1ac93b96123b91df932b0b8bd
kaelilor <kaelilor@students.zhaw.ch>
2019-11-04 15:11:29 +0100

    read in data in K-Means, cluster and predict

--- Code/K_Means_new.py
+++ Code/K_Means_new.py
@@ -2,60 +2,66 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
 import logfileparser as parser
+import NGramm as ng
 
 
 class K_Means_new(object):
     """description of class"""
-    k = 2
-    training_data = parser.read_data('../Logfiles/Labeled/normalTrafficTraining.txt')
 
-    #Test feature could be length of request.
-    #Parse data, extract requests and put their lengths in a list
-    requests = ["" for x in range(len(training_data))]
-    request_lengths = np.zeros([len(training_data)])
-    for i in range(len(training_data)):
-        tmp = training_data[i]
-        requests[i] = tmp.get('Request', None)
-        req = requests[i]
-        request_lengths[i] = len(req)
+    def __init__(self, k=1, tolerance=0.0001, max_iterations=500):
+        self.k = k
+        self.tolerance = tolerance
+        self.max_iterations = max_iterations
+        self.centroids = {}
 
-    print("\n**************************")
-    print("Training model:")
-    print("k = %d" %k)
+    def main():
+        # Reading Data
+        training_data = parser.read_data('../Logfiles/Labeled/normalTrafficTraining.txt')
+        test_clean = parser.read_data('../Logfiles/Labeled/normalTrafficTest.txt')
+        test_anomalous = parser.read_data('../Logfiles/Labeled/anomalousTrafficTest.txt')
+        training_data = parser.read_data('../Logfiles/Labeled/normalTrafficTraining.txt')
 
-    #fit trainings data to obtain clusters
-    kmeans = KMeans(k)
-    kmeans.fit(request_lengths)
+        # Training the N-Gramm extractor
+        ng = NGramm()
+        ng.fit(training_data)
+    
+        print("N-Gramms extracted!")
+        print("**************************")
+        print("Starting K-Means Fitting...")
 
-    clusters = kmeans.labels_.tolist()
 
-    for i in range(len(request_lengths)):
-        if cluster[i] == 1:
-            cluster1 = plt.scatter(request_lengths[i], c = 'r', marker = 'x')
-        elif clusters[i] == 0:
-            cluster2 = plt.scatter(request_lengths[i], c = 'g', marker = 'o')
+
+
+        # Getting Feature Vectors
+        training_vectors = ng.get_feature_vectors(training_data)
+        test_vectors_clean = ng.get_feature_vectors(test_clean)
+        test_vectors_anomalous = ng.get_feature_vectors(test_anomalous)
+
+        print("\n**************************")
+        print("Training model:")
+        print("k = %d" %k)
+
+        #fit trainings data to obtain clusters
+        kmeans = KMeans(k)
+        clusters = kmeans.fit(training_vectors)
+
+
+        for i in range(len(clusters)):
+            if cluster[i] == 1:
+                cluster1 = plt.scatter(request_lengths[i], c = 'r', marker = 'x')
+            elif clusters[i] == 0:
+                cluster2 = plt.scatter(request_lengths[i], c = 'g', marker = 'o')
             
 
-
-    plt.plot(clusters)
-
-    #test clean data
-    print("**************************")
-    print("Testing normal traffic:")
-    test_data = parser.read_data('../Logfiles/Labeled/normalTrafficTest.txt')
-    test_requests = []
-    test_request_lengths = []
-    malicious_requests = []
-
-    test_requests = ["" for x in range(len(training_data))]
-    test_request_lengths = np.zeros([len(training_data)])
-    for i in range(len(test_data)):
-        tmp = test_data[i]
-        test_requests[i] = tmp.get('Request', None) #returns None if key doesn't exist
-        test_req = test_requests[i]
-        test_request_lengths[i] = len(test_req)
+        #test clean data
+        print("Training done! Switch to testing.")
+        print("**************************")
+        print("Testing normal traffic:")
   
-    kmeans.predict(test_request_lengths)
+        result_clean = kmeans.predict(test_vectors_clean)
+        result_anomalous = kmeans.predict(test_vectors_anomalous)
+
+
 
 
 
