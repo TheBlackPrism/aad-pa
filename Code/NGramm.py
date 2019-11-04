c134e891e1e996f9cb4e9b33f17ee88a316d1b88
Yves Lütjens <yves@luetjens.ch>
2019-11-04 11:06:58 +0100

    Added Evaluation of the outlier detection and feature extraction

--- Code/NGramm.py
+++ Code/NGramm.py
@@ -1,6 +1,8 @@
 import re
 import numpy as np
 from sklearn.neighbors import LocalOutlierFactor
+import matplotlib as matplot
+import matplotlib.pyplot as plt
 import logfileparser as parser
 
 class NGramm():
@@ -76,10 +78,11 @@ class NGramm():
             occurences = sum(ngramms.values())
             vectors.append([probability, occurences])
             
-        return vectors
+        return np.asarray(vectors)
 
 def normalize_request(request):
     """Normalizes a request by replacing all alphanumeric characters with @
+    and removes all newline characters
     """
     regex = re.compile(r"[a-zA-Z0-9]+")
     replaced = re.sub(regex, '@',request)
@@ -88,46 +91,62 @@ def normalize_request(request):
     return replaced
 
 def main():
-    #read data here
+    # Reading Data
     training_data = parser.read_data('../Logfiles/Labeled/normalTrafficTraining.txt')
+    test_clean = parser.read_data('../Logfiles/Labeled/normalTrafficTest.txt')
+    test_anomalous = parser.read_data('../Logfiles/Labeled/anomalousTrafficTest.txt')
 
-    print("\n**************************")
+    print("**************************")
     print("Extracting N-Gramms...")
 
-    #fit data
+    # Training the N-Gramm extractor
     ng = NGramm()
     ng.fit(training_data)
     
     print("N-Gramms extracted!")
+    print("**************************")
+    print("Starting Local Outlier Fitting...")
 
-    """print("\n**************************")
-    print("All N-Gramms:")
-    print(ng.ngramms)
-    print("\n**************************")
-    print("N-Gramms probabilities:")
-    print(ng.ngramms_probability)
-    print("\n**************************")
-    print("Total N-Gramms:")
-    print(ng.total_number_ngramms)
-    """
 
-    print("\n**************************")
-    print("Starting K-Means...")
-
-    test_clean = parser.read_data('../Logfiles/Labeled/normalTrafficTest.txt')
-    test_anomalous = parser.read_data('../Logfiles/Labeled/anomalousTrafficTest.txt')
-
+    # Getting Feature Vectors
     training_vectors = ng.get_feature_vectors(training_data)
     test_vectors_clean = ng.get_feature_vectors(test_clean)
     test_vectors_anomalous = ng.get_feature_vectors(test_anomalous)
 
+    # Fitting model for novel predictions
     km = LocalOutlierFactor(novelty = True).fit(training_vectors)
+    
+    print("Fitting successful!")    
+    print("**************************")
+    print("Starting Prediction...")
 
-    # predict returns 1 for inlier and -1 for outlier
+    # Predict returns 1 for inlier and -1 for outlier
     result_clean = km.predict(test_vectors_clean)
     result_anomalous = km.predict(test_vectors_anomalous)
-    print(result_anomalous)
+    
+    print("Predicting successful!")    
+    print("**************************")
+    print("Results:")
 
+    # Evaluation
+    accuracy_anomalous = np.count_nonzero(result_anomalous == -1) / len(result_anomalous) * 100
+    accuracy_clean = np.count_nonzero(result_clean == 1) / len(result_clean) * 100
+
+    print("True Positiv: %d %%" % accuracy_anomalous)
+    print("False Positiv: %d %%" % (100 - accuracy_clean))
+    print("Accuracy: %d %%" % ((accuracy_anomalous * len(result_anomalous) + accuracy_clean * len(result_clean)) / (len(result_clean) + len(result_anomalous))))
+    
+    # Plotting Vectors
+    fig, ax = plt.subplots()
+    samples = 300
+    ax.scatter(training_vectors[:samples,0], training_vectors[:samples,1], s=200,color = "g", alpha = 0.5, label = "Trainings Data")
+    ax.scatter(test_vectors_clean[:samples,0], test_vectors_clean[:samples,1], s=150, color = "b", alpha = 0.5, label = "Clean Data")
+    ax.scatter(test_vectors_anomalous[:samples,0], test_vectors_anomalous[:samples,1], s=100, color = "r", alpha = 0.5, label = "Anomalous Data")
+    plt.xlabel("Probability of the Request")
+    plt.ylabel("Number of N-Gramms Occurences")
+    plt.title("Distribution of Feature Vectors")
+    ax.legend()
+    plt.show()
 
 if __name__ == "__main__":
     main()
