b4ab2cd57d658be958a24c48fd0099c7ece458da
kaelilor <kaelilor@students.zhaw.ch>
2019-11-25 14:29:22 +0100

    implementation DBSCAN

--- Code/DBSCAN.py
+++ Code/DBSCAN.py
@@ -11,24 +11,27 @@ test_anomalous = parser.read_data('../Logfiles/Labeled/anomalousTrafficTest.txt'
 
 # Training the N-Gramm extractor
 ng = NGram()
-ng.fit(training_data)
+training_data_url = ng.fit(training_data,True)
+training_data_param = ng.fit(training_data, False)
     
 print("N-Gramms extracted!")
 print("**************************")
-print("Starting K-Means Fitting...")
+print("Starting DB-Scan Fitting...")
 
 
 
 
 # Getting Feature Vectors
-training_vectors = ng.get_feature_vectors(training_data)
-test_vectors_clean = ng.get_feature_vectors(test_clean)
-test_vectors_anomalous = ng.get_feature_vectors(test_anomalous)
+training_vectors_url = ng.get_feature_vectors(training_data_url)
+training_vectors_param = ng.get_feature_vectors(traing_data_param)
+
+#test_vectors_clean = ng.get_feature_vectors(test_clean)
+#test_vectors_anomalous = ng.get_feature_vectors(test_anomalous)
 
 
 print("\n**************************")
 print("Training model:")
-print("eps = 0.5")
+print("eps = 0.1")
 print("minimum number of samples needed for a cluster: 3")
 
 #Building the clustering model
@@ -38,8 +41,31 @@ min_samples gives the minimum number of samples that must be found within
 eps to form a cluster.
 Both parameters must be chosen carefully, depending on the dataset.
 """
-dbscan = DBSCAN(eps = 0.5, min_samples=3).fit(training_vectors)
-labels = dbscan.labels_
+dbscan = DBSCAN(eps = 0.1, min_samples=3)
+model_url = dbscan.fit(training_vectors_url)
+model_param = dbscan.fit(training_vectors_param)
+y_pred_url = dbscan.fit_predict(training_vectors_url)
+y_pred_param = dbscan.fit_predict(training_vectors_param)
+labels_url = model_url.labels_
+labels_param = model_param.labels_
+
+#Identify cores
+#cores = np.zeros_like(labels, dtype=bool)
+#cores[dbscan.core_sample_indices_]=True
+
+#compute number of clusters
+#nbr_of_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+#print(nbr_of_clusters)
+
+plt.scatter(training_vectors_url[:,0], training_vectors_url[:,1], c = y_pred_url, cmap='Paired')
+plt.scatter(training_vectors_param[:,0], training_vectors_param[:,1], c = y_pred_param, cmap='Paired')
+
+
+
+plt.show()
+
+
+
 
 """Visualisation of clusters depends on the data in the dataset.
 
--- Code/NGram.py
+++ Code/NGram.py
@@ -78,6 +78,9 @@ class NGram():
         vectors = []
         for request in data:
             ngrams = self.get_ngrams_for_request(request)
+            if(len(ngrams)==0):
+                print(request)
+                print(ngrams)
             probability = self.get_probability_of_ngramset(ngrams)
             occurences = sum(ngrams.values())
             vectors.append([probability, occurences])
