b06f1e47a6e266a2f312cda74269179eb60730af
kaelilor <kaelilor@students.zhaw.ch>
2019-11-30 13:02:37 +0100

    add main method

--- Code/Anomaly_Detection.py
+++ Code/Anomaly_Detection.py
@@ -11,6 +11,18 @@ class Anomaly_Detection(object):
     def __init__(self):
         pass
 
+    def main():
+        # Reading Data
+        training_data = parser.read_data('../Logfiles/Labeled/normalTrafficTraining.txt')
+        test_clean = parser.read_data('../Logfiles/Labeled/normalTrafficTest.txt')
+        test_anomalous = parser.read_data('../Logfiles/Labeled/anomalousTrafficTest.txt')
 
+        training_data = parser.append_parameter_to_request(training_data)
+        test_clean = parser.append_parameter_to_request(test_clean)
+        test_anomalous = parser.append_parameter_to_request(test_anomalous)
+        print("**************************")
+
+if __name__ == "__main__":
+    main()
 
 
