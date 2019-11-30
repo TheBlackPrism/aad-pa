5a8cea2742034e4c0754381de452695cdfc69cc9
kaelilor <kaelilor@students.zhaw.ch>
2019-11-30 13:19:24 +0100

    add method to read from log files

--- Code/Anomaly_Detection.py
+++ Code/Anomaly_Detection.py
@@ -6,21 +6,38 @@ import K_Means_new
 import matplotlib as matplot
 import matplotlib.pyplot as plt
 
-class Anomaly_Detection(object):
+class Anomaly_Detection():
     """description of class"""
     def __init__(self):
         pass
 
-    def main():
-        # Reading Data
-        training_data = parser.read_data('../Logfiles/Labeled/normalTrafficTraining.txt')
-        test_clean = parser.read_data('../Logfiles/Labeled/normalTrafficTest.txt')
-        test_anomalous = parser.read_data('../Logfiles/Labeled/anomalousTrafficTest.txt')
+    def reading_data_from_file(self,path):
+        if(path == null):
+            print('Invalid path name. Please enter valid path...')
+            return
+        training_data = parser.read_data(path + '/normalTrafficTraining.txt')
+        test_clean = parser.read_data(path + '/normalTraffic.txt')
+        test_anomalous = parser.read_data(path + '/anomalousTrafficTest.txt')
 
         training_data = parser.append_parameter_to_request(training_data)
         test_clean = parser.append_parameter_to_request(test_clean)
         test_anomalous = parser.append_parameter_to_request(test_anomalous)
+
+        return training_data, test_clean, test_anomalous
+
+    def main():
+        ad = Anomaly_Detection()
+        print('Please enter the path of the logfiles...')
+        path = str(input())
         print("**************************")
+        print('Reading data...')
+
+        # Reading Data
+        training_data, test_clean, test_anomalous = ad.reading_data_from_file(path)
+
+        print("**************************")
+        print('Data read!')
+        print('Please enter the algorithm you would like to use...')
 
 if __name__ == "__main__":
     main()
