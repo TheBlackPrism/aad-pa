424778dc6be0285381ae048e045128f9747c9e13
kaelilor <kaelilor@students.zhaw.ch>
2019-11-30 12:57:24 +0100

    Create Anomaly_Detection.py

---
+++ Code/Anomaly_Detection.py
@@ -0,0 +1,16 @@
+import logfileparser as parser
+import numpy as np
+import NGram
+import outlier
+import K_Means_new
+import matplotlib as matplot
+import matplotlib.pyplot as plt
+
+class Anomaly_Detection(object):
+    """description of class"""
+    def __init__(self):
+        pass
+
+
+
+
